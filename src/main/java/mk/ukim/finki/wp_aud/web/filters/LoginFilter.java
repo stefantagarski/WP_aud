86947364e59ce9736006e22c331ba2ce976d975d
Stefan Tagarski <stefantagarski@gmail.com>
2024-10-27 15:05:07 +0100

    aud 3.1

---
+++ src/main/java/mk/ukim/finki/wp_aud/web/filters/LoginFilter.java
@@ -0,0 +1,40 @@
+package mk.ukim.finki.wp_aud.web.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import mk.ukim.finki.wp_aud.model.User;
+
+import java.io.IOException;
+
+@WebFilter
+public class LoginFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+        User user = (User) req.getSession().getAttribute("user");
+
+        String path = req.getServletPath();
+
+        if (user == null && !path.equals("/login")) {
+            resp.sendRedirect("/login");
+        } else {
+            filterChain.doFilter(req, resp);
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
